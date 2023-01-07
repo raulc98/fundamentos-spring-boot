996584abb7ea6656cdffe3b48b92ced5c95cfc17
Raul Cabrero Rouco <raulcabrerorouco@MacBook-Pro-de-Raul.local>
2023-01-07 23:50:23 +0100

    Primer commit

---
+++ src/test/java/com/fundamentos/springboot/fundamentos/FundamentosApplicationTests.java
@@ -0,0 +1,14 @@
+package com.fundamentos.springboot.fundamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundamentosApplicationTests {
+
+	//Todo lo relativo a pruebas unitarias en test
+	@Test
+	void contextLoads() {
+	}
+
+}
